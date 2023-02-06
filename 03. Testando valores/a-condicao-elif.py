c3782f9d13987848ab72f78b1829f9fe8a0f7f22
Juliana Virgens <julianalv1@hotmail.com>
2023-02-06 01:41:46 -0300

    Update a-condicao-elif.py
    Informação acrescentada.

--- 03. Testando valores/a-condicao-elif.py
+++ 03. Testando valores/a-condicao-elif.py
@@ -8,12 +8,12 @@ chute_do_usuario = int(input("Digite o seu número:"));
 
 print("Você digitou" , chute_do_usuario)
 
-acertou = numero_secreto == chute_do_usuario
+acertou = numero_secreto == chute_do_usuario #variável tipo bool = possui apenas dois valores: true e false.
 maior = chute_do_usuario > numero_secreto
 menor = chute_do_usuario < numero_secreto 
 
 if (acertou):
-    print("Você acertou!")
+    print("Parabéns, você acertou!")
 else:    
     if (maior):
         print("Você errou! O seu chute foi maior do que o número secreto.")
@@ -22,5 +22,5 @@ else:
         print("Você errou! O seu chute foi menor do que o número secreto.")
     #Podemos notar que, se o chute não for igual, nem maior que o número secreto, obviamente ele será menor, então o último if não é necessário.
     #Mas para esses casos, podemos fazer um else com uma condição de entrada, o elif. Vamos utilizá-lo para deixar o código mais semântico, já que na prática não há diferença.
-    
+
 print("Fim do jogo!")
