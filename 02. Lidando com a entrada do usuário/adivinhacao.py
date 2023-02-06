01148fb7d678fe73f1829e79a105bbf5ce0a76eb
Juliana Virgens <julianalv1@hotmail.com>
2023-02-06 00:49:16 -0300

    Update adivinhacao.py
    Acrescentei explicação sobre input.

--- 02. Lidando com a entrada do usuário/adivinhacao.py
+++ 02. Lidando com a entrada do usuário/adivinhacao.py
@@ -4,7 +4,7 @@ print("*********************************")
 
 numero_secreto = 34
 
-chute_do_usuario = int(input("Digite o seu número:")); # o input funciona para que receba uma mensagem para imprimir no console.
+chute_do_usuario = int(input("Digite o seu número:")); # o input funciona para que receba uma mensagem para imprimir no console. Pedir algo que o usuário deva digitar.
                                                        # o int faz com que seja lido um número, pois o input vai ler sempre uma string.
 
 print("Você digitou" , chute_do_usuario)
