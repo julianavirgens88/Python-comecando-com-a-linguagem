e8cc588f9a2301c5da128e40c6d4d21d1e689ef9
Juliana Virgens <julianalv1@hotmail.com>
2023-02-06 01:38:19 -0300

    Create if-else.py
    Correção exercício.

---
+++ 03. Testando valores/if-else.py
@@ -0,0 +1,25 @@
+#Corrigir:
+
+#usuario = input("Informe o usuário do sistema!")
+
+#if(usuario == "Flávio"):
+ #   print("Seja bem-vindo Flávio!")
+#else(usuario == "Douglas"):
+ #   print("Seja bem-vindo Douglas!")
+#else(usuario == "Nico"):
+ #   print("Seja bem-vindo Nico")
+#else:
+ #   print("Usuário não identificado!")
+
+ #Resolução
+ 
+usuario = input("Informe o usuário do sistema!")
+
+if(usuario == "Flávio"):
+    print("Seja bem-vindo Flávio!")
+elif(usuario == "Douglas"):
+    print("Seja bem-vindo Douglas!")
+elif(usuario == "Nico"):
+    print("Seja bem-vindo Nico")
+else:
+    print("Usuário não identificado!")
