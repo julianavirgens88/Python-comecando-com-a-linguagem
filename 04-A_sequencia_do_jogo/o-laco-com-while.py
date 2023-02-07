7de3b4cbf06f19e71c69ce6c0acc778877d15dca
Juliana Virgens <julianalv1@hotmail.com>
2023-02-07 00:51:00 -0300

    Update o-laco-com-while.py
    Exercício refeito.

--- 04-A_sequencia_do_jogo/o-laco-com-while.py
+++ 04-A_sequencia_do_jogo/o-laco-com-while.py
@@ -3,29 +3,27 @@ print("Bem vindo ao jogo de Adivinhacao!")
 print("*********************************")
 
 numero_secreto = 34
-
 total_de_tentativas = 3
-
 rodada = 1
 
-while(rodada <= total_de_tentativas):
+while (rodada <= total_de_tentativas):
     print("Tentativa", rodada, "de", total_de_tentativas)
-    chute_do_usuario = int(input("Digite o seu número:")) 
-    print("Você digitou" , chute_do_usuario)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
 
-acertou = numero_secreto == chute_do_usuario
-maior = chute_do_usuario > numero_secreto
-menor = chute_do_usuario < numero_secreto 
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (acertou):
-    print("Parabéns, você acertou!")
-else:    
-    if (maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
 
-rodada = rodada + 1
+    rodada = rodada + 1
 
-print("FIM!")
+print("Fim do jogo")
