85323965be409bb89c6f0bd05cde4d1361253822
Juliana Virgens <julianalv1@hotmail.com>
2023-02-06 22:23:02 -0300

    título
    Ajuste título.

---
+++ 01-Instalacao_do_Python3/exercicio.py
@@ -0,0 +1,14 @@
+print("ola mundo")
+
+print("Brasil ganhou 5 titulos mundiais")
+
+pais = "Italia"
+
+type(pais) # Imprime <class 'strig'> - indicando que é uma string.
+
+quantidade = 4
+
+type(quantidade) # Imprime <class 'int'> - Indicando que é um número inteiro.
+
+print("Brasil","ganhou",5,"titulos","mundiais", sep="-") # Imprime 'Brasil-ganhou-5-titulos-mundiais'.
+
---
+++ 04-A_sequencia_do_jogo/o-laco-com-while.py
@@ -0,0 +1,30 @@
+print("*********************************")
+print("Bem vindo ao jogo de Adivinhacao!")
+print("*********************************")
+
+numero_secreto = 34
+
+total_de_tentativas = 3
+
+rodada = 1
+
+while(rodada <= total_de_tentativas):
+    print("Tentativa", rodada, "de", total_de_tentativas)
+    chute_do_usuario = int(input("Digite o seu número:")) 
+    print("Você digitou" , chute_do_usuario)
+
+acertou = numero_secreto == chute_do_usuario
+maior = chute_do_usuario > numero_secreto
+menor = chute_do_usuario < numero_secreto 
+
+if (acertou):
+    print("Parabéns, você acertou!")
+else:    
+    if (maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    
+    elif(menor):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
+
+rodada = rodada + 1
+
